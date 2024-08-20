3b87e46d912440dd32110b35ede280741095eb2c
whitecar1 <vitalik.markovskiy.06@mail.ru>
2024-08-20 15:26:38 +0300

    I changed almost everything

--- PortScanner.py
+++ PortScanner.py
@@ -6,6 +6,7 @@ from datetime import datetime, date
 
 import sys
 import socket
+import time
 
 class PortScanner(QMainWindow):
     def __init__(self):
@@ -14,114 +15,118 @@ class PortScanner(QMainWindow):
         self.background = "708090"
         self.color = "#FFFF33"
         self.targetIp = None
-        self.minTargetPort = None
-        self.maxTargetPort = None
+        self.timer = 5
+
         
         self.createMenus()
         self.initUI()
         
         self.setWindowTitle("PortScanner")
         self.setGeometry(360, 170, 1200, 800)
-        self.setStyleSheet(f"background-color:{self.background}")
+        #dself.setStyleSheet(f"background-color:{self.background}")
         
     def initUI(self):
-        ipRange = "(?:[0-1]?[0-9]?[0-9]|2[0-4][0-9]|25[0-5])"
-        ipRegExp = QRegExp("^" + ipRange + "\\." + ipRange + "\\." + ipRange + "\\." + ipRange + "$")
-        ipValidator = QRegExpValidator(ipRegExp, self)
 
-        targetLayout = QHBoxLayout()
+        targetLayout = QGridLayout()
         targetLayout.setSpacing(20)
+
+        ipRange = "(?:[0-1]?[0-9]?[0-9]|2[0-4][0-9]|25[0-5])"
+        ipRegExp = QRegExp("^" + ipRange + "\\." + ipRange + "\\." + ipRange + "\\." + ipRange + "\\/" + ipRange + "$")
+        ipValidator = QRegExpValidator(ipRegExp, self)
         
-        targetLabel = QLabel("IP(s) range:")
-        targetLabel.setFont(QFont("Arial", 15))
-        targetLabel.setStyleSheet(f"color: {self.color}")
+        targetLabel = QLabel("IP addresses:")
+        targetLabel.setFont(QFont("Times", 15))
+        targetLabel.setStyleSheet(f"color: {self.color};")
         targetLabel.setAlignment(Qt.AlignLeft)
         
         self.targetEdit = QLineEdit()
-        self.targetEdit.setStyleSheet("background-color: white; color: red;")
+        self.targetEdit.setStyleSheet(f"background-color: white; color: black;")
         self.targetEdit.setValidator(ipValidator)
         self.targetEdit.setMaximumWidth(150)
-        #self.targetEdit.setAlignment(Qt.AlignCenter)
-        
-        toLabel = QLabel("to")
-        toLabel.setFont(QFont("Arial", 15))
-        toLabel.setStyleSheet(f"color: {self.color}")
-        toLabel.setAlignment(Qt.AlignLeft)
-
-        self.targetTwoEdit = QLineEdit()
-        self.targetTwoEdit.setStyleSheet("background-color: white; color: red;")
-        self.targetTwoEdit.setValidator(ipValidator)
-        self.targetTwoEdit.setMaximumWidth(150)
-        self.targetTwoEdit.setAlignment(Qt.AlignLeft)
-
-        orLabel = QLabel("or    /")
-        orLabel.setFont(QFont("Arial", 15))
-        orLabel.setStyleSheet(f"color: {self.color}")
-        orLabel.setAlignment(Qt.AlignCenter)
-        orLabel.setAlignment(Qt.AlignLeft)
-
-        self.cidrEdit = QLineEdit()
-        self.cidrEdit.setStyleSheet("background-color: white; color: red;")
-        self.cidrEdit.setValidator(ipValidator)
-        self.cidrEdit.setMaximumWidth(30)
-        self.cidrEdit.setAlignment(Qt.AlignLeft)
-        
-        targetLayout.addWidget(targetLabel)
-        targetLayout.addWidget(self.targetEdit)
-        targetLayout.addWidget(toLabel)
-        targetLayout.addWidget(self.targetTwoEdit)
-        targetLayout.addWidget(orLabel)
-        targetLayout.addWidget(self.cidrEdit)
 
         portRange = "([0-9]|[1-9][0-9]|[1-9][0-9][0-9]|[1-9][0-9][0-9][0-9]|[1-6][0-9][0-9][0-9][0-9])"
-        portRegExp = QRegExp("^" + portRange + "$")
+        portRegExp = QRegExp("^" + portRange + "-" + portRange + "$")
         portValidator = QRegExpValidator(portRegExp, self)
         
-        targetPortLabel = QLabel("Target port(s):")
-        targetPortLabel.setFont(QFont("Arial", 15))
-        targetPortLabel.setStyleSheet(f"color: {self.color}")
+        targetPortLabel = QLabel("Ports:")
+        targetPortLabel.setFont(QFont("Times", 15))
+        targetPortLabel.setStyleSheet(f"color: {self.color};")
 
         self.targetPortEdit = QLineEdit()
-        self.targetPortEdit.setStyleSheet("background-color: white; color: red;")
+        self.targetPortEdit.setStyleSheet("background-color: white; color: black;")
         self.targetPortEdit.setValidator(portValidator)
+        self.targetPortEdit.setMaximumWidth(150)
 
-        self.targetPortEdit = QLineEdit()
-        self.targetPortEdit.setStyleSheet("background-color: white; color: red;")
-        self.targetPortEdit.setValidator(portValidator)
+        targetVLayout = QGridLayout()
+        targetVLayout.addWidget(targetLabel, 0, 0)
+        targetVLayout.addWidget(self.targetEdit, 0, 1, 1, 2)
+        targetVLayout.addWidget(targetPortLabel, 1, 0)
+        targetVLayout.addWidget(self.targetPortEdit, 1, 1, 1, 2)
+
+        targetFrame = QFrame()
+        targetFrame.setFrameShape(QFrame.Box)
+        targetFrame.setFrameShadow(QFrame.Raised)
+        targetFrame.setLineWidth(3)
+        targetFrame.setStyleSheet(f"color: {self.color}; border: 2px solid {self.color}; border-radius: 5px;")
+        targetFrame.setLayout(targetVLayout)
+
+        targetLayout.addWidget(targetFrame, 0, 0, 1, 2)
+
+        optionsLayout = QGridLayout()
+
+        timerLabel = QLabel("Timer")
+        timerLabel.setFont(QFont("Times", 15))
+        timerLabel.setStyleSheet(f"color: {self.color}")
+
+        self.timerBox = QSpinBox()
+        self.timerBox.setRange(5, 60)
+        self.timerBox.setSingleStep(1)
+        self.timerBox.valueChanged.connect(self.TimerChanged)
+
+        optionsVLayout = QGridLayout()
+        optionsVLayout.addWidget(timerLabel, 0, 0, 1, 1)
+        optionsVLayout.addWidget(self.timerBox, 0, 1, 1, 1)
+
+        optionsFrame = QFrame()
+        optionsFrame.setFrameShape(QFrame.Box)
+        optionsFrame.setFrameShadow(QFrame.Raised)
+        optionsFrame.setLineWidth(3)
+        optionsFrame.setStyleSheet(f"color: {self.color}; border: 2px solid {self.color}; border-radius: 5px;")
+        optionsFrame.setLayout(optionsVLayout)
+
+        optionsLayout.addWidget(optionsFrame, 0, 0, 1, 2)
+
+        scanningButton = QPushButton("Start scanning")
+        scanningButton.setFont(QFont("Times", 15))
+        scanningButton.setStyleSheet(f"background-color: #FF00FF; color: {self.color};")
+        scanningButton.clicked.connect(self.startScanning)
+
+        self.outputText = QTextEdit()
+        self.outputText.setReadOnly(True)
+        self.outputText.setStyleSheet("background-color:white; color: black;")
+
+        '''
 
         saveButton = QPushButton("Save to file")
         saveButton.setFont(QFont("Arial", 15))
         saveButton.setStyleSheet("background-color: #CC0000; color: #FFD700;")
         saveButton.clicked.connect(self.saveToFile)
-        
-        scanningButton = QPushButton("Start scanning")
-        scanningButton.setFont(QFont("Arial", 15))
-        scanningButton.setStyleSheet("background-color: #FF00FF; color: #FFD700;")
-        scanningButton.clicked.connect(self.startScanning)
 
         exitButton = QPushButton("Exit")
         exitButton.setFont(QFont("Arial", 15))
         exitButton.setStyleSheet("background-color: #CC0000; color: #FFD700;")
         exitButton.clicked.connect(self.exitApp)
 
-        self.outputText = QTextEdit()
-        self.outputText.setReadOnly(True)
-        self.outputText.setStyleSheet("background-color:white; color: black;")
+        
+        '''
 
         mainLayout = QGridLayout()
-        mainLayout.addLayout(targetLayout, 0, 0)
-        '''
-        mainLayout.addWidget(targetLabel, 0, 0)
-        mainLayout.addWidget(self.targetEdit, 0, 1)
-        mainLayout.addWidget(toLabel, 0, 2)
-        mainLayout.addWidget(self.targetTwoEdit, 0, 3)
-        mainLayout.addWidget(targetPortLabel, 1, 0)
-        mainLayout.addWidget(self.targetPortEdit, 1, 1)
-        mainLayout.addWidget(saveButton, 3, 0)
-        mainLayout.addWidget(scanningButton, 3, 1)
-        mainLayout.addWidget(exitButton, 3, 2)
-        mainLayout.addWidget(self.outputText, 4, 0, 1, 3)
-        '''
+        
+        mainLayout.addLayout(targetLayout, 0, 1, 1, 2)  
+        mainLayout.addLayout(optionsLayout, 0, 3, 1, 2)   
+        mainLayout.addWidget(scanningButton, 1, 2, 1, 2)
+        mainLayout.addWidget(self.outputText, 2, 0, 1, 6)
+        
         centralWidget = QtWidgets.QWidget()
         centralWidget.setLayout(mainLayout)
         self.setCentralWidget(centralWidget)
@@ -151,6 +156,11 @@ class PortScanner(QMainWindow):
     def TargetOsChanged(self):
         return "not defined"
     
+    def TimerChanged(self):
+        self.timer = self.timerBox.value()
+        return self.timer
+        
+
     def saveToFile(self):
         filename, _ =QFileDialog.getSaveFileName(self, "Save file", "./", "Text file(*.txt);;All files(*.*)")
         
@@ -187,6 +197,7 @@ class PortScanner(QMainWindow):
                     sock.connect((target, port))
                     self.outputText.append(f"{port}     open")
                     sock.close()
+                    time.sleep(self.timer)
                 except:
                     pass
         else:
@@ -207,9 +218,9 @@ class PortScanner(QMainWindow):
             except:
                 pass
 
-
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     scanner = PortScanner()
     scanner.show()
     sys.exit(app.exec_())
+
