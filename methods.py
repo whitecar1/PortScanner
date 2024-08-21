611250cc98cf104ad43b83367680f109e3c1dd63
whitecar1 <vitalik.markovskiy.06@mail.ru>
2024-08-21 09:53:58 +0300

    Add defining of service by port

--- methods.py
+++ methods.py
@@ -1,12 +1,11 @@
 from PyQt5.QtGui import QTextCursor
 
+import socket
+
 def addValueToTable(text_editor, port, status="open"):
     text_editor.insertText(str(port))
     text_editor.movePosition(QTextCursor.NextCell)
     text_editor.insertText(status)
     text_editor.movePosition(QTextCursor.NextCell)
+    text_editor.insertText(socket.getservbyport(port))
     text_editor.movePosition(QTextCursor.NextCell)
-
-def countRowsInTable(rows:int):
-    rows += 1
-    return rows
